0b51084539e2699b526ac34d5c13f0a9cedc4bbd
Mohamed Eltorky <mohamed.eltorky1155@gmail.com>
2026-01-13 00:15:12 +0200

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:44359/api' // Update this with production URL when deploying
+    apiUrl: 'https://bookshub.tryasp.net/api' // Update this with production URL when deploying
 };
